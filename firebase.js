e2f98fdf2f2814f25775324ac75e79003ce5125c
Saswat <saswat.sam786@gmail.com>
2021-10-30 15:06:45 +0530

    setting up movienix

---
+++ firebase.js
@@ -0,0 +1,21 @@
+import firebase from "firebase";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+};
+
+// const app = initializeApp(firebaseConfig);
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+var provider = new firebase.auth.GoogleAuthProvider();
+const db = firebaseApp.firestore();
+const storage = firebase.storage();
+
+var auth = firebase.auth();
+export { db, storage, auth, provider };

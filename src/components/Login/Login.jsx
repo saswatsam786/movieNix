7a873688ebb3387f2e8b0a34ec39b1ec62339108
oyesaurav <78659500+oyesaurav@users.noreply.github.com>
2021-10-30 20:00:19 +0530

    Firebase setup

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,26 +2,33 @@
 import React from "react";
 import {Card, Button} from 'react-bootstrap';
 import "./login.css";
+import { auth, provider } from "../../firebase";
 
-export default function Login(){
-    const func = () =>{
-        window.location = "/";
-    }
-    return(
-        <div className="bg">
-            <div className="holder">
-                <Card className="text-center">
-                    <Card.Header>MovieNix Login</Card.Header>
-                    <Card.Body>
-                        <Card.Title>Login/Sign Up</Card.Title>
-                        <Card.Text>
-                        Login or create your account through Google
-                        </Card.Text>
-                        <Button variant="outline-primary" onClick={func}>Login</Button>
-                    </Card.Body>
-                    <Card.Footer className="text-muted">Powered By Google</Card.Footer>
-                </Card>
-            </div>  
-        </div>
-    );
+export default function Login() {
+  const signin = () => {
+    provider.setCustomParameters({ prompt: "select_account" });
+    auth.signInWithPopup(provider).catch(alert);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div className="bg">
+    <div className="holder">
+        <Card className="text-center">
+            <Card.Header>MovieNix Login</Card.Header>
+            <Card.Body>
+                <Card.Title>Login/Sign Up</Card.Title>
+                <Card.Text>
+                Login or create your account through Google
+                </Card.Text>
+                <Button variant="outline-primary" onClick={signin}>Login</Button>
+            </Card.Body>
+            <Card.Footer className="text-muted">Powered By Google</Card.Footer>
+        </Card>
+    </div>  
+    </div>
+  );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -14,8 +14,8 @@ const firebaseConfig = {
 // const app = initializeApp(firebaseConfig);
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 var provider = new firebase.auth.GoogleAuthProvider();
-const db = firebaseApp.firestore();
-const storage = firebase.storage();
+// const db = firebaseApp.firestore();
+// const storage = firebase.storage();
 
 var auth = firebase.auth();
-export { db, storage, auth, provider };
+export { auth, provider };

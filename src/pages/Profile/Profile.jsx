455dd31b55bf7468663944d20609c4fe85ed7162
Piyush Mishra <piyushmishra965@gmail.com>
2021-11-28 14:38:41 +0530

    working on login

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,12 +1,24 @@
 // eslint-disable-next-line
-import { Button, Card, ListGroup, ListGroupItem } from "react-bootstrap";
+// import { Button, Card, ListGroup, ListGroupItem } from "react-bootstrap";
 import React, { useState, useEffect } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth, db } from "../../firebase";
-import Home from "../Home/Home";
 // eslint-disable-next-line
-import axios, { Axios } from "axios";
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  CardActions,
+  Typography,
+  Button,
+  Grid,
+  Avatar,
+} from '@mui/material'
+import { createTheme } from "@mui/material/styles";
+import { ThemeProvider } from '@emotion/react';
 import { Link, useHistory } from "react-router-dom";
+import Home from "../Home/Home";
+import axios, { Axios } from "axios";
 import "./profile.css";
 
 export default function Profile() {
@@ -14,7 +26,13 @@ export default function Profile() {
   const [accid, setAccid] = useState("");
   const [accbal, setAccbal] = useState("");
   const [privatekey, setPrivatekey] = useState("");
-  const history = useHistory()
+  // const history = useHistory()
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: "dark",
+    },
+  });
 
   useEffect(() => {
     user &&
@@ -57,14 +75,88 @@ export default function Profile() {
 
     return (
       <div
-        className="profile-card"
         style={{
-          display: "flex",
-          justifyContent: "center",
-          padding: "50px",
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          padding: '70px',
         }}
+        className="profile-card"
       >
-        <Card
+        <ThemeProvider theme={darkTheme}>
+          <Grid container spacing={6}>
+            <Grid item xs={6} md={8}>
+              <Card 
+                elevation={2}
+                sx={{
+                  width: {xs: '230px', sm: '50%'},
+                  // maxWidth: 345,
+                  display: 'grid',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  padding: '50px'
+                }}>
+                <CardContent>
+                  <Avatar 
+                    src={user.photoURL}
+                    sx={{
+                      width: '200px',
+                      height: '200px',
+                      margin: 'auto'
+                    }}
+                    />
+                </CardContent>
+                <CardContent 
+                  sx={{
+                    margin: 'auto',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    textAlign: 'center'
+                  }} 
+                >
+                  <Typography variant="h2">
+                    {user.displayName}
+                  </Typography>
+                  <Typography variant="subtitle1" color="text.secondary" > 
+                    {user.email}
+                  </Typography>
+                  <div
+                    style={{
+                      marginTop: '50px'
+                    }}
+                  >
+                    <Link to="/library" className="navbar-link"><Button size="large">Library</Button></Link>
+                    <Link to="/" className="navbar-link"><Button onClick={logout} size="large">Logout</Button></Link>                  </div>
+                </CardContent>
+              </Card>
+            </Grid>
+            <Grid item>
+              <Card sx={{ 
+                // maxWidth: 345,
+                width: {xs: '230px', sm: '50%'},
+              }}>
+                <CardMedia
+                  component="img"
+                  alt="green iguana"
+                  height="140"
+                  image={user.photoURL}
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    Lizard
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Lizards are a widespread group of squamate reptiles, with over 6,000
+                    species, ranging across all continents except Antarctica
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small">Share</Button>
+                  <Button size="small">Learn More</Button>
+                </CardActions>
+              </Card>
+            </Grid>
+        {/* <Card
           style={{
             maxWidth: "300px",
             background: "rgb(54, 57, 64)",
@@ -91,7 +183,7 @@ export default function Profile() {
             </ListGroupItem>
           </ListGroup>
           <Card.Body>
-            {/* <Card.Link href="/">Home</Card.Link> */}
+            <Card.Link href="/">Home</Card.Link> 
             <Link to="/">Home</Link>
             <Link
               to="/"
@@ -102,19 +194,13 @@ export default function Profile() {
             >
               Logout
             </Link>
-            {/* <Card.Link href="/" onClick={logout}>
-              Logout
-            </Card.Link> */}
-            {/* <Button onClick={logout}>Logout</Button> */}
           </Card.Body>
-        </Card>
+        </Card> */}
+        </Grid>
+        </ThemeProvider>
       </div>
     );
   }
-
-  // function redirect12() {
-  //   window.location = "/";
-  // }
 
   return <>{loading ? <h1>loading...</h1> : user ? loadProfile() : <Home />}</>;
 }

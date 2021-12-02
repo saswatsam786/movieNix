9f9197b3a4aa0666839159fc8aae615e69f9f847
Saswat <saswat.sam786@gmail.com>
2021-12-02 15:14:09 +0530

    server deployment

--- server/server.js
+++ server/server.js
@@ -12,6 +12,7 @@ const {
 
 require("dotenv").config();
 const app = express();
+const port = process.env.PORT || 8000;
 app.use(
   cors({
     origin: "http://localhost:3000",
@@ -185,6 +186,6 @@ async function main() {
 }
 main();
 
-app.listen(8000, () => {
+app.listen(port, () => {
   console.log("SERVER RUNNING ON PORT 8000...");
 });
